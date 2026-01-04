4713b5073d25c4cbd8ebd292e145d3766c915779
YouYoungGeun <qwazsx1346@gmail.com>
2026-01-04 21:59:10 +0900

    주말 예습

--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: Home,
+      component: Home, //() => import('../views/Home.vue')
     },
     {
       path: '/memos/add',
